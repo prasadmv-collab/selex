14c4eee081859102f6a2ec8e8382a5880f3eac62
prasadmv-collab <prasadmvmsdpsd@gmail.com>
2026-06-17 11:54:06 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,39 +1,33 @@
 package com.example;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )throws InterruptedException 
+    public static void main(String[] args)
     {
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        
+        //  Add this block
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");   // required for Jenkins
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        //  Replace driver creation
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
         
-        Thread.sleep(2000);
-        
+        //  maximize may fail in headless, so optional
+        // driver.manage().window().maximize();
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(2000);
-        
-        
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        Thread.sleep(2000);
-        
         driver.findElement(By.id("login-button")).click();
-        Thread.sleep(2000);
-        
-        System.out.println("Login successfully done");
-        
-         Thread.sleep(3000);
-         
+
+        // good practice
         driver.quit();
-        
     }
 }
